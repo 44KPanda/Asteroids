bc4c9704d25a5f6f950875720c2c6d75a1f26411
44KPanda <44kpanda@gmail.com>
2025-04-18 15:02:16 +0900

    Added container class and full movements

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 
 def main():
@@ -10,16 +11,30 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    #print("Starting Asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    my_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
+        
+        for item in updatable:
+            item.update(dt)
+
+        for item in drawable:
+            item.draw(screen)
+        
+        #my_player.draw(screen)
+        #my_player.update(dt)
         pygame.display.flip()
         dt = (clock.tick(60)/1000)
+
+        #Prints FPS
         #print(1/dt)
 
 if __name__ == "__main__":
